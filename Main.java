9124773a0a06f2f856f0df14b12d7181ce9dabec
DimaVakarchuk <DeepVibes@yandex.ru>
2020-09-04 09:37:49 +0300

    Initial new code

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+                boolean boughtaticket = true;
+                int ticketcost = 2500;
+                int miles  = 20;
+                int bonus = ticketcost / miles;
+                System.out.println(bonus);
+
+            }
+        }
+
